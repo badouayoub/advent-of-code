4e30afb4b3f1b0cb1f6dce8568864ef190172ec8
badouayoub <badou.ayoub@gmail.com>
2024-12-01 19:49:39 +0900

    Day 1: Prob1

---
+++ 2024/day-1/prob1/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func fileToString(pathToFile string) string {
+	file, err := os.ReadFile(pathToFile)
+	if err != nil {
+		panic(err)
+	}
+	return string(file)
+}
+
+func parseDataToSortedArr(data string) ([]int, []int) {
+	var list1 []int
+	var list2 []int
+
+	rows := strings.Split(data, "\n")
+	for _, value := range rows {
+		ids := strings.Split(value, "   ")
+		for idx, id := range ids {
+			intId, err := strconv.Atoi(id)
+			if err != nil {
+				continue
+			}
+			if idx == 0 {
+				list1 = append(list1, intId)
+			} else if idx == 1 {
+				list2 = append(list2, intId)
+			}
+		}
+	}
+	slices.Sort(list1)
+	slices.Sort(list2)
+	return list1, list2
+}
+
+func main() {
+	data := fileToString("input.txt")
+	list1, list2 := parseDataToSortedArr(data)
+	fmt.Println(list1)
+	fmt.Println(list2)
+
+	totalDistance := 0
+	for i := range list1 {
+		sum := list1[i] - list2[i]
+		totalDistance += int(math.Abs(float64(sum)))
+	}
+	fmt.Println(totalDistance)
+}
